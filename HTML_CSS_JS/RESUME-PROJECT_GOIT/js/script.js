886796e4123a17714403241b58412992a75a730e
Andrey <s1avyan27@gmail.com>
2020-04-20 04:51:30 +0300

    Horisontal Slider done.

---
+++ HTML_CSS_JS/RESUME-PROJECT_GOIT/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $('.slider').slick({
+    arrows: false,
+    dots: true,
+    adaptiveHeight: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    centerMode: false,
+    speed: 1000,
+    variableWidth: false,
+  });
+});
